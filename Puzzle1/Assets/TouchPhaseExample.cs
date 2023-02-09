0a396aeaa494ac591c29b2a417a39aea5bbe3ad9
Sam-exe <sammiedelbammie@gmail.com>
2023-02-09 17:36:17 +0100

    Start Project

--- Puzzle1/Assets/TouchPhaseExample.cs
+++ Puzzle1/Assets/TouchPhaseExample.cs
@@ -10,20 +10,34 @@ public class TouchPhaseExample : MonoBehaviour
 {
     public Vector2 startPos;
     public Vector2 direction;
+    public Vector2 direction2;
 
+    public Vector2 worldPosition;
+    public Text m_Text2;
     public Text m_Text;
     string message;
-
+    public GameObject Prefab;
     void Update()
     {
         //Update the Text on the screen depending on current TouchPhase, and the current direction vector
-        m_Text.text = "Touch : " + message + "in direction" + direction;
-
+        m_Text.text = "Touch : " + startPos + "in direction" + direction;
+        
+        foreach(Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                  //new Vector2(screenspace.x, Screen.height - screenspace.y) // where screenspace is the Vector2 in screenspace coordinates
+                worldPosition = Camera.main.ScreenToWorldPoint(touch.position);
+                Instantiate(Prefab, new Vector3(worldPosition.x, worldPosition.y, -1), Quaternion.identity);
+            }
+        }
+        
         // Track a single touch as a direction control.
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-
+            transform.position = new Vector2(direction.x / Screen.height, direction.y / Screen.width);
+            //Touch test = Input.tou
             // Handle finger movements based on TouchPhase
             switch (touch.phase)
             {
@@ -36,6 +50,7 @@ public class TouchPhaseExample : MonoBehaviour
 
                 //Determine if the touch is a moving touch
                 case TouchPhase.Moved:
+                    direction2 = touch.position;
                     // Determine direction by comparing the current touch position with the initial one
                     direction = touch.position - startPos;
                     message = "Moving ";
