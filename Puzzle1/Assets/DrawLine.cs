19d873fb21be9a0f1954fbd59eafb865d0fb1b45
Sam-exe <sammiedelbammie@gmail.com>
2023-02-17 09:53:36 +0100

    Small Changes

--- Puzzle1/Assets/DrawLine.cs
+++ Puzzle1/Assets/DrawLine.cs
@@ -18,11 +18,7 @@ public class DrawLine : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        LineRenderer
-
-        LineRenderer.SetPosition(0, finger0.transform.GetChild(1).position);
-        LineRenderer. (1, new Vector3(finger0.transform.GetChild(1).position.x, finger0.transform.GetChild(1).position.y, -1));
-        Debug.DrawRay(finger0.transform.GetChild(1).position,finger0.transform.GetChild(1).position, Color.black);
+        
         
     }
 }
