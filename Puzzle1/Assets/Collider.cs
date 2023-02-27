86bea401bca4f8d871f638aeca9409e2a259b00a
Sam-exe <sammiedelbammie@gmail.com>
2023-02-27 11:00:51 +0100

    Edits

--- Puzzle1/Assets/Collider.cs
+++ Puzzle1/Assets/Collider.cs
@@ -17,7 +17,7 @@ public class Collider : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         for (int i = 0; i < lr.positionCount; i++)
         {
